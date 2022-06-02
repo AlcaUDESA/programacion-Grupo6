df0cda8178093909fcfa670adcb6f6f31430c1bc
Gabuxto <gcarrionmayorga@udesa.edu.ar>
2022-06-02 18:37:42 -0300

    login y logout v1

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const database = require('../db/database')
+
+const bcrypt = require('bcryptjs');
+
 const userController = {
     show: (req,res)=>{
         return res.render('profile', {
@@ -8,6 +11,38 @@ const userController = {
     },
     login: (req, res) => {
         return res.render('login')
+    
+    },
+
+    procesarLogin : (req, res) => {
+        let info = req.body;
+        let filtro = {where : [ { email : info.email}]};
+
+        user.findOne(filtro)
+        .then((result) => {
+
+            if (result != null) {
+
+                let passEncriptada = bcrypt.compareSync(info.password , result.password)
+                if (passEncriptada) {
+
+                    req.session.user = result.dataValues;
+
+                    if (req.body.remember != undefined) {
+                        res.cookie('userId', result.dataValues.id, {maxAge : 1000 * 60 *10 } )
+                    }
+
+                    return res.redirect("/movies/all")
+                } else {
+                    return res.send("el usuario con el mail " +  result.email + " existe pero la clave es incorrecta");
+                }
+
+            } else {
+                return res.send("No esta registrado" +  info.email);
+            }
+        }).catch((err) => {
+
+        });
     },
     register: (req,res) =>{
         return res.render('register')
@@ -16,7 +51,13 @@ const userController = {
         return res.render('profile-edit',{
             usuario: database.usuario
         })
-    }
+    },
+
+    logout : (req, res) => {
+        req.session.destroy();
+        res.clearCookie('userId');
+        return res.render("login")
+    },
 }
 
 module.exports = userController
