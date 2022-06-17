9ca22341f6168a9c12c3ec1876601c9a1870497e
AlcaUDESA <agustinlucass03@gmail.com>
2022-06-17 15:04:49 -0300

    search results primer approach, v1 sin corregir algunas variables

--- controller/mainController.js
+++ controller/mainController.js
@@ -24,6 +24,23 @@ const mainController = {
         results: results,
       })}
     )},
+  searchResults: (req,res) =>{
+    //Promesa findAll para traer todos los resultados de busqueda posibles de productos//
+    products.findAll({
+      //Incluimos la asociacion con usuario y comentarios//
+      include: [{association: 'User'}, {association: 'Coments'}],
+      //Utilizamos un where con operador para que la búsqueda sea pertinente//
+      where: [{model: {[op.like]: '%'+ req.query.search + '%' }}],
+    })
+      .then((results)=>{
+      //Renderizamos la vista de Search Resultsy le pasamos a la vista informacion de los results de la promesa//
+      return res.render('search-results', {
+          results: results,
+          query: req.query.search,
+        });
+      }).catch((errors) =>{console.log(errors)})
+      
+  },
 }
 
 module.exports = mainController;
